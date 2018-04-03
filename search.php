b20f7a1e18bf8aabc255ab244898c3d5cb330809
SuryaChaitanya1998 <psuryachaitanya@gmail.com>
2018-04-03 14:45:27 +0530

    Modified search.php

--- search.php
+++ search.php
@@ -1,19 +1,49 @@
-<?php
-	$con=mysqli_connect("localhost","root","password","data");
-	$query = "SELECT filename from questionpapers where";
-	if ($isset($_POST['sem'])){
-		$query = $query + ' semester = ' + $_POST['semester'];
+	<?php
+	require 'connection.php';
+	$conn = connect();
+	$myquery = "SELECT filename FROM `questionpapers` WHERE";
+	$flag=0;
+	if (isset($_POST['semester'])){
+		//echo "1";
+		if($flag==0)
+		{
+				$myquery = $myquery . ' semester = ' ."'".$_POST['semester']."'" ;
+				$flag=1;
+		}else {
+				$myquery = $myquery. ' AND' . ' semester = ' ."'". $_POST['semester']."'"  ;
+		}
 	}
-	if ($isset($_POST['branch'])){
-		$query = $query + ' branch = ' + $_POST['branch'];
+	if (isset($_POST['branch'])){
+		//echo "2";
+		if($flag==0)
+		{
+			$myquery = $myquery . ' branch = ' ."'".$_POST['branch']."'";
+			$flag=1;
+		}else{
+			$myquery = $myquery .' AND'. ' branch = ' ."'". $_POST['branch']."'";
+		}
 	}
-	if ($isset($_POST['school'])){
-		$query = $query + ' subject = ' + $_POST['subject'];
+	if (isset($_POST['subject'])){
+		//echo "3";
+		if($flag==0)
+		{
+			$myquery = $myquery . ' subject = ' ."'". $_POST['subject']."'";
+			$flag=1;
+		}else {
+			$myquery = $myquery .' AND'. ' subject = ' ."'". $_POST['subject']."'";
+		}
 	}
-	if ($isset($_POST['subject'])){
-		$query = $query + ' school = ' + $_POST['school'];
+	if(isset($_POST['school'])){
+		if($flag==0)
+		{
+				$myquery = $myquery . ' school = ' ."'". $_POST['school']."'";
+				$flag=1;
+		}else {
+				$myquery = $myquery .' AND'. ' school = ' ."'". $_POST['school']."'";
+
+		}
 	}
-	$result = mysqli_query($con,$query);
+	$result = $conn->query($myquery);
 	$array = array();
 	while (mysqli_num_rows($result) > 0){
 		array_push($array,$result['filename']);
