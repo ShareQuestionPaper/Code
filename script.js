430e78b5b1b9d0d925bdcda7fbaf3e337847d9d1
Sandesh Ghanta <sghanta05@gmail.com>
2018-04-22 16:12:19 +0530

    Updated UI

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 defaultfunc();
 function defaultfunc(){
-    console.log("Good");
     check();
     load_most_contributed();
-    console.log("Bad");
+    load_recently_uploaded();
+    view("hello");
 }
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
@@ -31,6 +31,29 @@ function search(){
     xhttp.open("GET", "search.php?query="+query, true);
     xhttp.send();
 }
+function load_recently_uploaded() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange=function(){
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            var table = document.getElementById("ru");
+            for (var i=0;i<data.length;i++){
+                var row = table.insertRow(i+1);
+                var cell0 = row.insertCell(0)
+                var cell1 = row.insertCell(1);
+                var cell2 = row.insertCell(2);
+                var atag = document.createElement('a');
+                atag.setAttribute('href',"sharestuff.com?viewpaper="+data[i]['link']);
+                atag.innerHTML = data[i]['name'];
+                cell0.innerHTML = i+1; 
+                cell1.appendChild(atag);
+                cell2.innerHTML = data[i]['time'];
+            }
+        };
+    }
+    xhttp.open("GET", "recently_uploaded.json", true);
+    xhttp.send();
+}
 function load_most_contributed() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange=function(){
@@ -53,4 +76,9 @@ function load_most_contributed() {
     }
     xhttp.open("GET", "most_contributed.json", true);
     xhttp.send();
+}
+function view(url){
+    url = "data.pdf";
+    var data = "<div class='embed-responsive' style='padding-bottom:150%'><object data='"+url+"' type='application/pdf' width='100%' height='100%'></object></div>";
+    document.getElementById("main").innerHTML = data;
 }
